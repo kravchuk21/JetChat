58cb7107c417376b0aa7d4f54e87968c83b80794
VladKrauchuk <krauchukvlad@gmail.com>
2022-05-15 22:26:11 +0300

    Init

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Hello Jet</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
