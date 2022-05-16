3af62e4f9231e6e58d81abaf3950737f4fbb351d
VladKrauchuk <krauchukvlad@gmail.com>
2022-05-16 19:06:49 +0300

    Avatar Component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import Avatar from './src/components/Avatar';
 
 const App = () => {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Text>Hello Jet</Text>
+      <Avatar
+        fullName="Vladislav Kravchuk"
+        // avatar="https://images.unsplash.com/photo-1652669253736-1e815c884373?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+        avatar={null}
+      />
     </SafeAreaView>
   );
 };
---
+++ src/utils/helpers/generateAvatarFromHash.ts
@@ -0,0 +1,23 @@
+import tinyColor from 'tinycolor2';
+
+const getCorrectIndex = (number: number) => {
+  if (number > 255) {
+    return 255;
+  }
+  if (number < 0) {
+    return 0;
+  }
+  return number > 255 ? 255 : number < 0 ? 0 : number;
+};
+
+export default (hash: string) => {
+  const [r, g, b] = hash
+    .substr(0, 3)
+    .split('')
+    .map(char => getCorrectIndex(char.charCodeAt(0)));
+
+  return {
+    color: tinyColor({r, g, b}).lighten(10).saturate(10).toHexString(),
+    colorLighten: tinyColor({r, g, b}).lighten(30).saturate(30).toHexString(),
+  };
+};
