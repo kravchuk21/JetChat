6cd6474e930773e7d3ab0d14d0f7c3d432033cd1
VladKrauchuk <krauchukvlad@gmail.com>
2022-05-20 19:17:17 +0300

    Input Component

--- src/screens/Screen1.tsx
+++ src/screens/Screen1.tsx
@@ -9,8 +9,41 @@ import {Content, Screen} from '../../styled';
 import IconButton from '../components/IconButton';
 import GoBackSvgIcon from '../assets/svg/GoBackSvgIcon';
 import Bubble from '../components/Bubble';
+import {Formik, Field} from 'formik';
+import CustomInput from '../components/Input';
+import * as yup from 'yup';
 
 type Screen1NavigationProps = NativeStackScreenProps<RootStackParamList>;
+
+const signUpValidationSchema = yup.object().shape({
+  fullName: yup
+    .string()
+    .matches(/(\w.+\s).+/, 'Enter at least 2 names')
+    .required('Full name is required'),
+  phoneNumber: yup
+    .string()
+    .matches(/(01)(\d){8}\b/, 'Enter a valid phone number')
+    .required('Phone number is required'),
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .matches(/\w*[a-z]\w*/, 'Password must have a small letter')
+    .matches(/\w*[A-Z]\w*/, 'Password must have a capital letter')
+    .matches(/\d/, 'Password must have a number')
+    .matches(
+      /[!@#$%^&*()\-_"=+{}; :,<.>]/,
+      'Password must have a special character',
+    )
+    .min(8, ({min}) => `Passowrd must be at least ${min} characters`)
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords do not match')
+    .required('Confirm password is required'),
+});
 
 const Screen1: React.FC<Screen1NavigationProps> = ({navigation}) => {
   return (
@@ -85,6 +118,56 @@ const Screen1: React.FC<Screen1NavigationProps> = ({navigation}) => {
             text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, nam nihil quod repellendus sunt voluptas! Blanditiis dignissimos enim facere fugiat harum id inventore minus officia, perferendis porro praesentium, quia quidem!"
             date="12:34"
           />
+          <Formik
+            validationSchema={signUpValidationSchema}
+            initialValues={{
+              fullName: '',
+              email: '',
+              phoneNumber: '',
+              password: '',
+              confirmPassword: '',
+            }}
+            onSubmit={values => console.log(values)}>
+            {({handleSubmit, isValid, values}) => (
+              <>
+                <Field
+                  component={CustomInput}
+                  name="fullName"
+                  placeholder="Full Name"
+                />
+                <Field
+                  component={CustomInput}
+                  name="email"
+                  placeholder="Email Address"
+                  keyboardType="email-address"
+                />
+                <Field
+                  component={CustomInput}
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  keyboardType="numeric"
+                />
+                <Field
+                  component={CustomInput}
+                  name="password"
+                  placeholder="Password"
+                  secureTextEntry
+                />
+                <Field
+                  component={CustomInput}
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  secureTextEntry
+                />
+
+                <Button
+                  onClick={handleSubmit}
+                  disabled={!isValid || values.email === ''}>
+                  SignUp
+                </Button>
+              </>
+            )}
+          </Formik>
         </Content>
       </ScrollView>
     </Screen>
